cada6bee6d80c85a8992ae7466e7552b4d26a9ff
Nicholas Easler <easlern@gmail.com>
2023-01-26 20:48:32 -0500

    Adding tests

--- test_BinaryTree.py
+++ test_BinaryTree.py
@@ -28,7 +28,31 @@ class MyTestCase(unittest.TestCase):
         tree.add([-1], 'testItem2')
         leaves = tree.getLeaves()
         s = self.leavesListToString(leaves)
-        self.assertSame('', s)
+        self.assertSame(['[-1] testItem2', '[0] testItem1'], s)
+
+    def test_addThreeItemsOneDimension_leftThenLeftThenLeft(self):
+        tree = BinaryTree(1)
+        tree.add([0], 'testItem1')
+        tree.add([-1], 'testItem2')
+        tree.add([1], 'testItem3')
+        leaves = tree.getLeaves()
+        s = self.leavesListToString(leaves)
+        self.assertSame(['[-1] testItem2', '[0] testItem1', '[1] testItem3'], s)
+
+    def test_addOneItemTwoDimensions(self):
+        tree = BinaryTree(2)
+        tree.add([0,0], 'testItem1')
+        leaves = tree.getLeaves()
+        s = self.leavesListToString(leaves)
+        self.assertSame(['[0, 0] testItem1'], s)
+
+    def test_addTwoItemsTwoDimensions(self):
+        tree = BinaryTree(2)
+        tree.add([0, 0], 'testItem1')
+        tree.add([1, 0], 'testItem2')
+        leaves = tree.getLeaves()
+        s = self.leavesListToString(leaves)
+        self.assertSame(['[0, 0] testItem1', '[1, 0] testItem2'], s)
 
     def leavesListToString(self, leavesList):
         return str(list(map(lambda l: str(l.LocationAsArray) + ' ' + str(l.Item), leavesList)))
