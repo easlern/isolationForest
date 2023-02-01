461de751c8faead46f05569f7047b357d3227062
Nicholas Easler <easlern@gmail.com>
2023-02-01 08:59:17 -0500

    Trying out a partition algorithm

--- scrap.py
+++ scrap.py
@@ -1,4 +1,6 @@
 def getDensity(l):
+    if len(l) == 1:
+        return None
     l.sort()
     start = l[0]
     end = l[-1]
@@ -6,9 +8,25 @@ def getDensity(l):
     return len(l)*1.0 / w
 
 def getPartition(l):
-    for c in range(2, len(l)-1):
+    vals = dict()
+    for c in range(1, len(l)):
         a = l[:c]
         b = l[c:]
         print('** ' + str(c))
-        print(str(getDensity(a)))
-        print(str(getDensity(b)))
+        d0 = getDensity(a)
+        d1 = getDensity(b)
+        print(str(d0))
+        print(str(d1))
+        if d0 is None:
+            vals[c] = d1
+        elif d1 is None:
+            vals[c] = d0
+        else:
+            vals[c] = d0*d1
+    best = None
+    for key in vals:
+        k = key
+        v = vals[k]
+        if best is None or v > best[1]:
+            best = (k,v)
+    print('I choose ' + str(best[0]))
